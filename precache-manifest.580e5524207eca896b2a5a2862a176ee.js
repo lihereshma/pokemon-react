9985113cd46641549ede3a9a0c57f6fd2c31756d
lihereshma <reshmalihe00068@gmail.com>
2020-09-19 14:28:13 +0530

    Updates

---
+++ precache-manifest.580e5524207eca896b2a5a2862a176ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c550203796db7d7ea956f8fc638516f6",
+    "url": "/pokemon-react/index.html"
+  },
+  {
+    "revision": "9045aa9ffd8fcc5cb8c6",
+    "url": "/pokemon-react/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bd240711553c300c524c",
+    "url": "/pokemon-react/static/css/main.4116817a.chunk.css"
+  },
+  {
+    "revision": "9045aa9ffd8fcc5cb8c6",
+    "url": "/pokemon-react/static/js/2.13abe5f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-react/static/js/2.13abe5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd240711553c300c524c",
+    "url": "/pokemon-react/static/js/main.12c1d1d2.chunk.js"
+  },
+  {
+    "revision": "06976b6d33b8ad8580ee",
+    "url": "/pokemon-react/static/js/runtime-main.bc88ce2f.js"
+  }
+]);
