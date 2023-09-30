e9c6714c27bf5bd8724ad9cb218189e78a308016
lihereshma <54063216+lihereshma@users.noreply.github.com>
2023-09-30 20:54:03 +0530

    Images updated

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -5,7 +5,7 @@ export default function Pokemon({ name, id }) {
     <li className="col-lg-3 col-md-4 col-sm-6">
       <h2>{ name }</h2>
       <h3>#{ id }</h3>
-      <img src={ `https://pokeres.bastionbot.org/images/pokemon/${ id }.png` } loading="lazy" alt={ name } />
+      <img src={ `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${ id }.svg` } loading="lazy" alt={ name } />
     </li>
   )
 }
